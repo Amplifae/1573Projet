77f9cfb11a7a9329a5a5b39ee5981f1e70d7ad2b
Amplifae <42949843+Amplifae@users.noreply.github.com>
2022-03-24 19:28:07 -0400

    graph class

--- src/Graph.java
+++ src/Graph.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+
+
+
 
 public class Graph {
 
+    ArrayList<Node> nodes = new ArrayList<Node>();
+    ArrayList<Edge> edges = new ArrayList<Edge>();
+
+
+    public void displayMap() {
+        nodes.add(new Node(10,10,"N1"));
+        nodes.add(new Node(20,10,"N2"));
+        nodes.add(new Node(30,10,"N3"));
+        nodes.add(new Node(10,30,"N4"));
+        nodes.add(new Node(20,30,"N5"));
+        nodes.add(new Node(30,30,"N6"));
+
+
+    }
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.displayMap();
+
+    }
 }
