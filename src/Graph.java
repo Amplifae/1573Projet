d3bfd502d9a36036d146f21c56fe0bdd835e2659
Samuel Hein <97979982+SamHein8@users.noreply.github.com>
2022-04-03 19:49:17 -0400

    Add files via upload

--- src/Graph.java
+++ src/Graph.java
@@ -10,29 +10,67 @@ public class Graph {
     // https://gyazo.com/539ad025df8a0a2dad5f697b122df346
 
     public static void main(String[] args) {
-        Node n0 = new Node("0");            // Noeuds qui vont composer le graphique
-        Node n1 = new Node("1");
-        Node n2 = new Node("2");
-        Node n3 = new Node("3");
-        Node n4 = new Node("4");
-        Node n5 = new Node("5");
+    	
+        Node nA = new Node("A");            // Noeuds qui vont composer le graphique
+        Node nB = new Node("B");
+        Node nC = new Node("C");
+        Node nD = new Node("D");
+        Node nE = new Node("E");
+        Node nF = new Node("F");
+        Node nG = new Node("G");            // Noeuds qui vont composer le graphique
+        Node nH = new Node("H");
+        Node nI = new Node("I");
+        Node nJ = new Node("J");
+        Node nK = new Node("K");
+        Node nL = new Node("L");
+        Node nM = new Node("M");            // Noeuds qui vont composer le graphique
+        Node nN = new Node("N");
+        Node nO = new Node("O");
+        Node nP = new Node("P");
 
-        n0.addParent(new Edge(n0, n1, 2)); // On ajoute les chemins entre les noeuds
-        n0.addParent(new Edge(n0, n3, 3));
-        n0.addParent(new Edge(n0, n5, 8));
-        n1.addParent(new Edge(n1, n0, 2));
-        n1.addParent(new Edge(n1, n2, 1));
-        n1.addParent(new Edge(n1, n3, 6));
-        n1.addParent(new Edge(n1, n4, 5));
-        n2.addParent(new Edge(n2, n1, 1));
-        n2.addParent(new Edge(n2, n5, 2));
-        n3.addParent(new Edge(n3, n0, 3));
-        n3.addParent(new Edge(n3, n1, 6));
-        n4.addParent(new Edge(n4, n1, 5));
-        n4.addParent(new Edge(n4, n5, 3));
-        n5.addParent(new Edge(n5, n0, 8));
-        n5.addParent(new Edge(n5, n2, 2));
-        n5.addParent(new Edge(n5, n4, 3));
+        nA.addParent(new Edge(nA, nB, 5)); // On ajoute les chemins entre les noeuds
+        nA.addParent(new Edge(nA, nG, 10));
+        nB.addParent(new Edge(nB, nC, 5));
+        nB.addParent(new Edge(nB, nF, 10));
+        nB.addParent(new Edge(nB, nA, 5));
+        nC.addParent(new Edge(nC, nE, 10));
+        nC.addParent(new Edge(nC, nB, 5));
+        nC.addParent(new Edge(nC, nD, 5));
+        nD.addParent(new Edge(nD, nC, 5));
+        nD.addParent(new Edge(nD, nJ, 10));
+        nE.addParent(new Edge(nE, nC, 10));
+        nE.addParent(new Edge(nE, nH, 5));
+        nE.addParent(new Edge(nE, nF, 5));
+        nF.addParent(new Edge(nF, nE, 5));
+        nF.addParent(new Edge(nF, nB, 10));
+        nF.addParent(new Edge(nF, nG, 5));
+        nG.addParent(new Edge(nG, nF, 5));
+        nG.addParent(new Edge(nG, nA, 10));
+        nG.addParent(new Edge(nG, nI, 5));
+        nH.addParent(new Edge(nH, nE, 5));
+        nH.addParent(new Edge(nH, nJ, 5));
+        nH.addParent(new Edge(nH, nI, 10));
+        nH.addParent(new Edge(nH, nL, 5));
+        nI.addParent(new Edge(nI, nG, 5));
+        nI.addParent(new Edge(nI, nM, 5));
+        nI.addParent(new Edge(nI, nH, 10));
+        nJ.addParent(new Edge(nJ, nD, 10));
+        nJ.addParent(new Edge(nJ, nK, 5));
+        nJ.addParent(new Edge(nJ, nH, 5));
+        nK.addParent(new Edge(nK, nJ, 5));
+        nK.addParent(new Edge(nK, nL, 5));
+        nK.addParent(new Edge(nK, nN, 5));
+        nL.addParent(new Edge(nL, nK, 5));
+        nL.addParent(new Edge(nL, nM, 10));
+        nL.addParent(new Edge(nL, nH, 5));
+        nM.addParent(new Edge(nM, nL, 10));
+        nM.addParent(new Edge(nM, nI, 5));
+        nM.addParent(new Edge(nM, nO, 5));
+        nN.addParent(new Edge(nN, nK, 5));
+        nN.addParent(new Edge(nN, nO, 20));
+        nO.addParent(new Edge(nO, nM, 5));
+        nO.addParent(new Edge(nO, nN, 20));
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Interface().setVisible(true);
@@ -48,10 +86,16 @@ public class Graph {
 				}
 			}
 		});*/
+        
+        
 
         Pathing smollPath = new Pathing();
-        smollPath.findPath(n1);                             // On met le noeud de départ
-        System.out.println(smollPath.SmallPath(n5));        // On met le noeud d'arrivée
+        smollPath.findPath(nA);                             // On met le noeud de départ
+        System.out.println(smollPath.SmallPath(nN));        // On met le noeud d'arrivée
+        //smollPath.SmallPath(nN).toString();
+        Interface.AfficherTrajet(smollPath.SmallPath(nN).toString());
+        
+        
 
     }
 }
